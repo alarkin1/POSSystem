3e90cc10bad8d29fc34f54e31345395d6af21f8f
Alex <Alex@MajorFox>
2015-02-25 17:09:00 -0600

    Created classes CashRegister, Customer, LineItem, Sale, Startup.

---
+++ src/possystem/CashRegister.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package possystem;
+
+/**
+ *
+ * @author Alex
+ */
+public class CashRegister {
+
+    //private ReceiptPrinter receiptPrinter = new ReceiptPrinter();
+    //private CustomerReader customerReader = new CustomerReader();
+
+    public CashRegister() {
+    }
+
+    public void processLineItems(LineItem[] customersLineItems) {
+        for (LineItem lineItem : customersLineItems) {
+            
+        }
+    }
+
+}
---
+++ src/possystem/Customer.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package possystem;
+
+/**
+ *
+ * @author Alex
+ */
+public class Customer {
+
+    private String customerID;
+    private LineItem[] customersLineItems;
+
+    public Customer(String customerID, LineItem[] customersLineItems) {
+        this.customerID = customerID;
+        this.customersLineItems = customersLineItems;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public LineItem[] getCustomersLineItems() {
+        return customersLineItems;
+    }
+
+    public void setCustomersLineItems(LineItem[] customersLineItems) {
+        this.customersLineItems = customersLineItems;
+    }
+
+}
